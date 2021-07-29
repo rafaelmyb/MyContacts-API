b4a26a9a28ca9f1b00718928262a64d4406d9b26
Rafael Yamada <rafaelyamada321@gmail.com>
2021-07-29 18:10:17 -0400

    :tada: first commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,48 @@
+const ContactsRepository = require('../repositories/ContactsController');
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: not found
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    response.json(contact);
+  }
+
+  store() {
+    // Criar novo registro
+  }
+
+  update() {
+    // Editar um registro
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: not found
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    await ContactsRepository.delete(id);
+    // 204: No Content
+    response.sendStatus(204);
+  }
+}
+
+// Singleton
+module.exports = new ContactController();
