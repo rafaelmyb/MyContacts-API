7c3e930acab7750fcf44433ffb569bcde5c562f2
Rafael Yamada <rafaelyamada321@gmail.com>
2021-08-09 10:08:17 -0400

    refactoring others Repository methods

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -2,7 +2,8 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response) {
-    const contacts = await ContactsRepository.findAll();
+    const { orderBy } = request.query;
+    const contacts = await ContactsRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -73,13 +74,6 @@ class ContactController {
 
   async delete(request, response) {
     const { id } = request.params;
-
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: not found
-      return response.status(404).json({ error: 'Contact not found' });
-    }
 
     await ContactsRepository.delete(id);
     // 204: No Content
